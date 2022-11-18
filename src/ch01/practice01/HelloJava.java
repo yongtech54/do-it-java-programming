572b9fbd26b84d5189d18d10389ab7aa57b50265
yongtech54 <yongtech54@gmail.com>
2022-11-18 22:31:20 +0900

    Create: CH01 실습
    - package단위로 간단한 프로그램 작성

---
+++ src/ch01/practice01/HelloJava.java
@@ -0,0 +1,7 @@
+package src.ch01.practice01;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
---
+++ src/ch01/practice02/HelloWorld.java
@@ -0,0 +1,7 @@
+package src.ch01.practice02;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("\"Hello, Yongtaek Lee!\"");
+    }
+}
