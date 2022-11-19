0af781fc21c5a5b8204ac1347ff16540ae7a9733
yongtech54 <yongtech54@gmail.com>
2022-11-20 00:58:30 +0900

    Create: package
    - CH01 실습
    - 패키지 이동

--- src/ch01/practice/Q6.java
+++ src/ch01/practice/Q6.java
@@ -1,6 +1,6 @@
-package src.ch01;
+package src.ch01.practice;
 
-public class HelloWorld {
+public class Q6 {
     public static void main(String[] args) {
         System.out.println("\"Hello, Yongtaek Lee!\"");
     }
